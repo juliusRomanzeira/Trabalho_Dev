3c55c5edefe825467f62a94d11546b67ad6ffcae
JuliusRomanzeira <julius.romanzeira@gmail.com>
2025-05-14 14:45:40 -0300

    Request

---
+++ pagina-de-acesso/back-end/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request,response)=> {
+    return response.json({
+        nome: 'Julius',
+        idade: 22,
+        profissao: 'Engenheiro',
+    });
+});
+app.listen(3333);
